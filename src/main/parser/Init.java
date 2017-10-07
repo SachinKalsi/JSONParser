0098584eed2ba2dbfc169c11b8397000e63ecedf
kalsi <sachinkalsi15@gmail.com>
2017-10-07 19:13:27 +0530

    added getkey function

--- src/main/parser/Init.java
+++ src/main/parser/Init.java
@@ -6,13 +6,29 @@ import java.util.*;
  * Created by kalsi on 07/10/17.
  */
 public class Init {
+    static Map<String, Object> json = null;
 
     public static void main(String[] args) {
-        String json = "{\"hey\":[{\"then\":\"nothing\"}],\"sample\":{\"not\":\"ok\",\"ok\":\"rascal\"},\"color\":\"red\",\"size\":10,\"cost\":100.0,\"big\":true}";
-        Map<String, Object> map = Parser.parse(json);
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
+        String jsonString = "{\"color\":\"red\",\"size\":10,\"cost\":100.0,\"mattress\":{\"name\":\"king\"},\"big\":true,\"legs\":[{\"length\":4}]}";
+        json = Parser.parse(jsonString);
+//        for (Map.Entry<String, Object> entry : json.entrySet()) {
+//            System.out.print(entry.getKey() + " : ");
+//            System.out.println(entry.getValue());
+//        }
+        findKey("size", "10");
+    }
+
+    public static void findKey(String key, String value) {
+        Object map = json;
+        String[] array = key.split("\\.");
+        for (String keyString : array) {
+            map = getValue(keyString, map);
         }
+        System.out.println(map == value);
+    }
+
+    public static Object getValue(String key, Object map) {
+        map = (Map<String, Object>) map;
+        return  ((Map<String, Object>) map).get(key);
     }
 }
